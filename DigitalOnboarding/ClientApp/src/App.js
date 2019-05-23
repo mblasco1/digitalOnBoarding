3c31443f17d104df1176cd1d6e13c8b82b1f34e6
Fabio Strappazzon <Fabio.Strappazzon@ti8m.ch>
2019-05-23 10:41:52 +0200

    update node packages add microblink

--- DigitalOnboarding/ClientApp/src/App.js
+++ DigitalOnboarding/ClientApp/src/App.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
+import { Tabs, Tab } from 'react-bootstrap';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import { Phone } from './components/Phone';
+import { IdCheck } from './components/IdCheck';
+import { Address } from './components/Address';
 
 export default class App extends Component {
   displayName = App.name
 
   render() {
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetchdata' component={FetchData} />
-      </Layout>
+        <Tabs defaultActiveKey="profile">
+            <Tab eventKey="phone" title="Phone">
+                <Phone />
+            </Tab>
+            <Tab eventKey="adress" title="Address">
+                <Address />
+            </Tab>
+            <Tab eventKey="idcheck" title="ID Check">
+                <IdCheck />
+            </Tab>
+        </Tabs>
     );
   }
 }
--- DigitalOnboarding/ClientApp/src/components/NavMenu.js
+++ DigitalOnboarding/ClientApp/src/components/NavMenu.js
@@ -1,6 +1,6 @@
 ﻿import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Glyphicon, Nav, Navbar, NavItem } from 'react-bootstrap';
+import { Nav, Navbar, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import './NavMenu.css';
 
@@ -20,17 +20,17 @@ export class NavMenu extends Component {
           <Nav>
             <LinkContainer to={'/'} exact>
               <NavItem>
-                <Glyphicon glyph='home' /> Home
+                Home
               </NavItem>
             </LinkContainer>
             <LinkContainer to={'/counter'}>
               <NavItem>
-                <Glyphicon glyph='education' /> Counter
+                 Counter
               </NavItem>
             </LinkContainer>
             <LinkContainer to={'/fetchdata'}>
               <NavItem>
-                <Glyphicon glyph='th-list' /> Fetch data
+                Fetch data
               </NavItem>
             </LinkContainer>
           </Nav>
