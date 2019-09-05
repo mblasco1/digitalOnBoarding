3c9127f724d7c439b64793c38b71dc25b4d7c361
PhilippWetzel <47518256+PhilippWetzel@users.noreply.github.com>
2019-09-05 11:39:32 +0200

    change IDBild to random

--- DigitalOnboarding/ClientApp/src/pages/onBoardingProcess/welcome.jsx
+++ DigitalOnboarding/ClientApp/src/pages/onBoardingProcess/welcome.jsx
@@ -39,7 +39,7 @@ const Welcome = (props) => {
 				<TimLogo className={classes.logo} />
             </Fab>
             <br></br>
-            <div>Version: 1.0.1.24 </div>
+            <div>Version: 1.0.1.27 </div>
 		</div>
 	);
 
